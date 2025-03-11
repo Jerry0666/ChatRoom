6f6bd1a67fc7290707c70abf5b06ef4489e57103
Jerry <zzherui54@gmail.com>
2025-03-11 11:55:14 +0800

    init commit

---
+++ ChatRoomServer/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace ChatRoomServer
+{
+    partial class Server
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            txtIP = new TextBox();
+            txtPort = new TextBox();
+            StartBtn = new Button();
+            label3 = new Label();
+            InfoBox = new TextBox();
+            StatusLab = new Label();
+            CloseBtn = new Button();
+            BtnList = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(93, 65);
+            label1.Name = "label1";
+            label1.Size = new Size(36, 25);
+            label1.TabIndex = 0;
+            label1.Text = "IP:";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(93, 110);
+            label2.Name = "label2";
+            label2.Size = new Size(59, 25);
+            label2.TabIndex = 1;
+            label2.Text = "Port:";
+            // 
+            // txtIP
+            // 
+            txtIP.Location = new Point(201, 62);
+            txtIP.Name = "txtIP";
+            txtIP.Size = new Size(125, 33);
+            txtIP.TabIndex = 2;
+            txtIP.Text = "127.0.0.1";
+            // 
+            // txtPort
+            // 
+            txtPort.Location = new Point(201, 107);
+            txtPort.Name = "txtPort";
+            txtPort.Size = new Size(125, 33);
+            txtPort.TabIndex = 3;
+            txtPort.Text = "13000";
+            // 
+            // StartBtn
+            // 
+            StartBtn.Location = new Point(103, 163);
+            StartBtn.Name = "StartBtn";
+            StartBtn.Size = new Size(97, 32);
+            StartBtn.TabIndex = 4;
+            StartBtn.Text = "Start";
+            StartBtn.UseVisualStyleBackColor = true;
+            StartBtn.Click += StartBtn_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(93, 219);
+            label3.Name = "label3";
+            label3.Size = new Size(196, 25);
+            label3.TabIndex = 5;
+            label3.Text = "User's information:";
+            // 
+            // InfoBox
+            // 
+            InfoBox.Location = new Point(93, 261);
+            InfoBox.Multiline = true;
+            InfoBox.Name = "InfoBox";
+            InfoBox.ScrollBars = ScrollBars.Vertical;
+            InfoBox.Size = new Size(660, 394);
+            InfoBox.TabIndex = 6;
+            InfoBox.WordWrap = false;
+            // 
+            // StatusLab
+            // 
+            StatusLab.AutoSize = true;
+            StatusLab.ForeColor = SystemColors.ButtonShadow;
+            StatusLab.Location = new Point(481, 62);
+            StatusLab.Name = "StatusLab";
+            StatusLab.Size = new Size(95, 25);
+            StatusLab.TabIndex = 8;
+            StatusLab.Text = "Stopped";
+            // 
+            // CloseBtn
+            // 
+            CloseBtn.ForeColor = Color.Red;
+            CloseBtn.Location = new Point(843, 611);
+            CloseBtn.Name = "CloseBtn";
+            CloseBtn.Size = new Size(101, 44);
+            CloseBtn.TabIndex = 9;
+            CloseBtn.Text = "Close";
+            CloseBtn.UseVisualStyleBackColor = true;
+            CloseBtn.Click += CloseBtn_Click;
+            // 
+            // BtnList
+            // 
+            BtnList.Location = new Point(256, 163);
+            BtnList.Name = "BtnList";
+            BtnList.Size = new Size(182, 32);
+            BtnList.TabIndex = 10;
+            BtnList.Text = "List all user";
+            BtnList.UseVisualStyleBackColor = true;
+            BtnList.Click += BtnList_Click;
+            // 
+            // Server
+            // 
+            AutoScaleDimensions = new SizeF(12F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1135, 721);
+            Controls.Add(BtnList);
+            Controls.Add(CloseBtn);
+            Controls.Add(StatusLab);
+            Controls.Add(InfoBox);
+            Controls.Add(label3);
+            Controls.Add(StartBtn);
+            Controls.Add(txtPort);
+            Controls.Add(txtIP);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Font = new Font("Microsoft JhengHei UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 136);
+            Margin = new Padding(4);
+            Name = "Server";
+            Text = "Server";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox txtIP;
+        private TextBox txtPort;
+        private Button StartBtn;
+        private Label label3;
+        private TextBox InfoBox;
+        private Label StatusLab;
+        private Button CloseBtn;
+        private Button BtnList;
+    }
+}
---
+++ ChatRoomServer/Form1.cs
@@ -0,0 +1,313 @@
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ChatRoomServer
+{
+
+    public partial class Server : Form
+    {
+        public Server()
+        {
+            InitializeComponent();
+            UserDict = new Dictionary<string, ClientInfo>();
+            RoomDict = new Dictionary<string, ChatRoom>();
+        }
+
+        private delegate void delUpdateUI(string sMessage);
+        private delegate void delWriteTextBox(TextBox t, string s);
+
+        TcpListener m_server;
+        Thread m_thrListening;
+        Dictionary<string, ClientInfo> UserDict;
+        Dictionary<string, ChatRoom> RoomDict;
+        private void StartBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int nPort = Convert.ToInt32(txtPort.Text);
+                IPAddress localAddr = IPAddress.Parse(txtIP.Text);
+
+                m_server = new TcpListener(localAddr, nPort);
+                m_server.Start();
+                m_thrListening = new Thread(Listening);
+                m_thrListening.Start();
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine("SocketException: {0}", ex);
+            }
+        }
+
+        private void Listening()
+        {
+            try
+            {
+                while (true)
+                {
+                    UpdateStatus("Waiting for connection...");
+                    TcpClient client = m_server.AcceptTcpClient();
+                    UpdateStatus("Connect to client!");
+                    Thread cThread = new Thread(() => ClientThread(client));
+                    cThread.Start();
+                    Thread.Sleep(1000);
+                }
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine("SocketException: {0}", ex);
+            }
+        }
+
+        private void ClientThread(TcpClient client)
+        {
+            NetworkStream stream = client.GetStream();
+            byte[] btDatas = new byte[512];
+            string sData;
+            WriteSomething(InfoBox, "InClientThread");
+            string clinetName = "";
+            string roomName = "";
+            while (true)
+            {
+                try
+                {
+                    int len = stream.Read(btDatas, 0, btDatas.Length);
+                    sData = System.Text.Encoding.ASCII.GetString(btDatas, 0, len);
+                    WriteSomething(InfoBox, sData);
+                    string action;
+                    action = sData.Substring(0, 5);
+                    string remain = sData.Substring(5);
+                    string[] words = remain.Split(" ");
+                    byte[] messByte;
+                    switch (action)
+                    {
+                        case "[reg]":
+                            // do user registeration.
+                            if (UserDict.ContainsKey(words[0]))
+                            {
+                                byte[] result = System.Text.Encoding.ASCII.GetBytes("register failure: The user name is already in use.");
+                                stream.Write(result);
+                            }
+                            else
+                            {
+                                ClientInfo info = new ClientInfo();
+                                info.password = words[1];
+                                UserDict.Add(words[0], info);
+                                info.name = words[0];
+                                info.stream = stream;
+                                byte[] result = System.Text.Encoding.ASCII.GetBytes("register successfully");
+                                stream.Write(result);
+                            }
+                            break;
+                        case "[log]":
+                            // authenticate the user
+                            if (UserDict.ContainsKey(words[0]))
+                            {
+                                string name = UserDict[words[0]].password;
+                                if (String.Compare(name, words[1]) == 0)
+                                {
+                                    // authenticate success
+                                    byte[] result = System.Text.Encoding.ASCII.GetBytes("log in success");
+                                    UserDict[words[0]].LoggedIn = true;
+                                    clinetName = words[0];
+                                    stream.Write(result);
+                                }
+                                else
+                                {
+                                    byte[] result = System.Text.Encoding.ASCII.GetBytes("Wrong password");
+                                    stream.Write(result);
+                                }
+                            }
+                            else
+                            {
+                                byte[] result = System.Text.Encoding.ASCII.GetBytes("Wrong password");
+                                stream.Write(result);
+                            }
+                            break;
+                        case "[ent]":
+                            roomName = words[0];
+                            if (RoomDict.ContainsKey(roomName))
+                            {
+                                // push user into the room list
+                                RoomDict[roomName].Users.Add(clinetName);
+                                byte[] result = System.Text.Encoding.ASCII.GetBytes("[Success] Room exist, come into the room.");
+                                stream.Write(result);
+                                UserDict[clinetName].roomName = roomName;
+                            }
+                            else
+                            {
+                                // create the chat room
+                                ChatRoom room = new ChatRoom();
+                                room.roomName = roomName;
+                                if (String.Compare(clinetName, "") != 0)
+                                {
+                                    room.Users.Add(clinetName);
+                                }
+                                else
+                                {
+                                    // error, should not happen 
+                                }
+                                RoomDict.Add(roomName, room);
+                                byte[] result = System.Text.Encoding.ASCII.GetBytes("[Success] Room not exist, create one.");
+                                stream.Write(result);
+                                WriteSomething(InfoBox, "stream write");
+                                UserDict[clinetName].roomName = roomName;
+                            }
+                            break;
+                        case "[sen]":
+                            // find the chat room
+                            string rName = UserDict[clinetName].roomName;
+                            string message = "";
+                            message += "[info]";
+                            message += clinetName;
+                            message += "(Me):";
+                            message += remain;
+                            message += Environment.NewLine;
+                            messByte = System.Text.Encoding.ASCII.GetBytes(message);
+                            stream.Write(messByte);
+                            // Write to all member in the caht room.
+                            foreach (string s in RoomDict[rName].Users)
+                            {
+                                if (String.Compare(s, clinetName) != 0)
+                                {
+                                    string mess = "";
+                                    mess += "[info]";
+                                    mess += clinetName;
+                                    mess += ":";
+                                    mess += remain;
+                                    mess += Environment.NewLine;
+                                    byte[] messBy = System.Text.Encoding.ASCII.GetBytes(mess);
+                                    UserDict[s].stream.Write(messBy);
+                                }
+                            }
+                            break;
+                        case "[lar]":
+                            string allroom = "";
+                            foreach (var item in RoomDict)
+                            {
+                                allroom += item.Key;
+                                allroom += ", ";
+                            }
+                            if (allroom.Length == 0)
+                            {
+                                allroom += "no room!";
+                            }
+                            messByte = System.Text.Encoding.ASCII.GetBytes(allroom);
+                            stream.Write(messByte);
+                            break;
+
+                        case "[lur]":
+                            string alluser = "[user]";
+                            WriteSomething(InfoBox, roomName);
+                            foreach (string s in RoomDict[roomName].Users)
+                            {
+                                alluser += s;
+                                alluser += ", ";
+                            }
+                            messByte = System.Text.Encoding.ASCII.GetBytes(alluser);
+                            stream.Write(messByte);
+                            break;
+                        case "[exi]":
+                            RoomDict[roomName].Users.Remove(clinetName);
+                            roomName = "";
+                            UserDict[clinetName].roomName = "";
+                            break;
+                        case "[out]":
+                            UserDict[clinetName].LoggedIn = false;
+                            break;
+                    }
+                }
+                catch (Exception e)
+                {
+                    UpdateStatus("read exception:" + e.Message);
+                    client.Close();
+                    break;
+                }
+            }
+        }
+
+        private void UpdateStatus(string sStatus)
+        {
+            if (this.InvokeRequired)
+            {
+                delUpdateUI del = new delUpdateUI(UpdateStatus);
+                this.Invoke(del, sStatus);
+            }
+            else
+            {
+                StatusLab.Text = sStatus;
+            }
+        }
+
+        private void WriteSomething(TextBox t, string s)
+        {
+            if (this.InvokeRequired)
+            {
+                delWriteTextBox del = new delWriteTextBox(WriteSomething);
+                this.Invoke(del, t, s);
+            }
+            else
+            {
+                t.Text = s;
+            }
+        }
+
+        private void CloseBtn_Click(object sender, EventArgs e)
+        {
+            m_server.Dispose();
+            Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnList_Click(object sender, EventArgs e)
+        {
+            // List all the user in the system
+            string allUser = "";
+            foreach (var item in UserDict)
+            {
+                if (item.Value.LoggedIn == false)
+                {
+                    continue;
+                }
+                allUser += "User: ";
+                allUser += item.Key;
+                if (item.Value.roomName.Length > 0)
+                {
+                    allUser += "  in the room [";
+                    allUser += item.Value.roomName;
+                    allUser += "]";
+                }
+                allUser += Environment.NewLine;
+            }
+            WriteSomething(InfoBox, allUser);
+        }
+    }
+}
+
+class ClientInfo
+{
+    public string name = "";
+    public string password = "";
+    public bool LoggedIn = false;
+    public string roomName = "";
+    public NetworkStream stream;
+}
+
+class ChatRoom
+{
+    public string roomName = "";
+    public List<string> Users;
+    public ChatRoom()
+    {
+        Users = new List<string>();
+    }
+}
